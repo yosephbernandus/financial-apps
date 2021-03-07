b50c13458debf08b5b3f122e9369cf2cc17aacb7
yosephbernandus <yosephbernandus@gmail.com>
2021-03-07 18:01:26 +0700

    add django cors headers

--- financial_server/settings.py
+++ financial_server/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,9 +49,13 @@ INSTALLED_APPS = [
     'financial_server.apps.transactions',
 ]
 
+# TODO: Whitelist from server react url
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
