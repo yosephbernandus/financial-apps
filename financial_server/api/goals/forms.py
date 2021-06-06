71f930d2d628562c9c85cbbdc104b4dbb7d8fd5d
Yoseph Bernandus <yosephbernandus@gmail.com>
2021-06-06 21:42:49 +0700

    add saving goals api add (#19)

---
+++ financial_server/api/goals/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from django.utils import timezone
+
+from dateutil import parser
+from typing import Dict, Any
+
+from financial_server.apps.users.models import User
+from financial_server.apps.categories.models import Category
+from financial_server.apps.financial_goals.models import FinancialGoal
+
+
+class AddSavingsGoal(forms.Form):
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    amount = forms.IntegerField()
+    name = forms.CharField()
+    achievement_date = forms.CharField()
+    deposit_cycle = forms.IntegerField()
+
+    def __init__(self, user: User, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.user = user
+
+    def clean(self) -> Dict:
+        if self.errors:
+            return self.cleaned_data
+
+        amount = self.cleaned_data['amount']
+        name = self.cleaned_data['name']
+        achievement_date = self.cleaned_data['achievement_date']
+
+        try:
+            achievement_date = parser.parse(achievement_date).date()
+        except ValueError:
+            raise forms.ValidationError("%s bukan format yang benar" % achievement_date)
+
+        if achievement_date < timezone.localdate():
+            raise forms.ValidationError("date of achievement of funds can not be less than today")
+
+        if amount < 0:
+            raise forms.ValidationError("amount cannot be less than 0")
+
+        if not name:
+            raise forms.ValidationError("name cannot be empty")
+
+        return self.cleaned_data
+
+    def save(self) -> FinancialGoal:
+        goal: FinancialGoal = FinancialGoal.objects.create(
+            user=self.user,
+            category=self.cleaned_data['category'],
+            amount=self.cleaned_data['amount'],
+            name=self.cleaned_data['name'],
+            achievement_date=parser.parse(self.cleaned_data['achievement_date']).date(),
+            deposit_cycle=self.cleaned_data['deposit_cycle']
+        )
+
+        return goal
--- financial_server/api/goals/views.py
+++ financial_server/api/goals/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import status
+from rest_framework import response
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from financial_server.api.response import ErrorResponse
 from financial_server.api.views import FinancialAPIView
-
 from financial_server.apps.financial_goals.models import FinancialGoal
-
 from financial_server.core.serializers import serialize_financial_goals
+
+from .forms import AddSavingsGoal
 
 
 class Sync(FinancialAPIView):
@@ -27,3 +29,14 @@ class Details(FinancialAPIView):
     def get(self, request: Request, id: int) -> Response:
         goal = get_object_or_404(FinancialGoal, user=request.user, id=id)
         return Response(serialize_financial_goals(goal), status=status.HTTP_200_OK)
+
+
+class AddSavings(FinancialAPIView):
+    def post(self, request: Request) -> Response:
+        form = AddSavingsGoal(data=request.data, user=request.user)
+
+        if form.is_valid():
+            goal = form.save()
+            return response(serialize_financial_goals(goal), status=status.HTTP_200_OK)
+
+        return ErrorResponse(form=form)
