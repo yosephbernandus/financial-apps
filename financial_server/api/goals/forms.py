dd6b9a9bf953bb2d2d9371392a3b3324e7813a05
Yosephbernandus <yosephbernandus@gmail.com>
2021-08-22 21:46:06 +0700

    Fix create goal

--- financial_server/api/goals/forms.py
+++ financial_server/api/goals/forms.py
@@ -45,18 +45,15 @@ class EditGoalForm(forms.Form):
         return self.cleaned_data
 
     def save(self) -> FinancialGoal:
-        goal: FinancialGoal = FinancialGoal.objects.update_or_create(
-            user=self.user,
-            defaults= {
-                'category': self.cleaned_data['category'],
-                'amount': self.cleaned_data['amount'],
-                'goal_name': self.cleaned_data['name'],
-                'achievement_date': parser.parse(self.cleaned_data['achievement_date']).date(),
-                'deposit_cycle': self.cleaned_data['deposit_cycle']
-            }
+        goal = self.user.financial_goals.create(
+            category=self.cleaned_data['category'],
+            amount=self.cleaned_data['amount'],
+            goal_name=self.cleaned_data['name'],
+            achievement_date=parser.parse(self.cleaned_data['achievement_date']).date(),
+            deposit_cycle=self.cleaned_data['deposit_cycle']
         )
 
-        return goal[0]
+        return goal
 
 
 class EditGoalSavingTransactionForm(forms.ModelForm):
