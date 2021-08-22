7251fb0b8ecd41c017ff5716fdeafd2228d3e8c2
Yosephbernandus <yosephbernandus@gmail.com>
2021-08-22 21:51:02 +0700

    fix view admin

--- financial_server/apps/financial_goals/admin.py
+++ financial_server/apps/financial_goals/admin.py
@@ -4,6 +4,7 @@ from .models import FinancialGoal, GoalSavingsTransaction
 
 
 class FinancialGoalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'goal_name', 'amount', 'achievement_date')
     search_fields = ("goal_name", "user__name")
     autocomplete_fields = ("user",)
 
