048016ce9ea0cc40864b9a5fdcaee789fd3fad27
Yosephbernandus <yosephbernandus@gmail.com>
2021-10-03 14:48:55 +0700

    add django extensions

--- financial_server/settings.py
+++ financial_server/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django_extensions',
     'thumbnails',
 
     'financial_server.apps.permissions',
